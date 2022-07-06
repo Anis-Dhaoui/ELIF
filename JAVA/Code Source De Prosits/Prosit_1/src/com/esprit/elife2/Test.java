2d91e372b8300ff07f2e426a359f07e5335ff526
anis dhaoui <freeconcept6@gmail.com>
2022-07-06 22:58:58 +0100

    Ajout de Prosit 1 (Correction de Prof)

---
+++ JAVA/Code Source De Prosits/Prosit_1/src/com/esprit/elife2/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.esprit.elife2;
+
+/**
+ *
+ * @author aissa
+ */
+public class Test {
+    public int a;
+    private int b;
+    protected int c;
+    int d;
+}
